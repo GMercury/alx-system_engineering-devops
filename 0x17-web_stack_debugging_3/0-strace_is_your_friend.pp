7528367f7b986c1edae7024a197b8e1720fee0e3
GMercury <kingorigithinji@gmail.com>
2022-11-02 20:25:59 +0300

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Fixes a wordpress site running on apache2
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+# Corrects phpp extension for php
+exec {'phppFix':
+    path    => '/bin',
+    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
 }
